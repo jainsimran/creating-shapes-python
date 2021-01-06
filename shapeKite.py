a16df371f47e97c0b201b9541085a2806502710b
--replace-all <itsimmi5@gmail.com>
2021-01-05 22:34:41 -0500

    feat: added kite shape

---
+++ shapeKite.py
@@ -0,0 +1,18 @@
+import turtle
+screen = turtle.Screen()
+screen.bgcolor('blue')
+sam = turtle.Turtle()
+sam.pencolor('white')
+sam.pensize(4)
+sam.left(60)
+sam.forward(180)
+
+sam.left(80)
+sam.forward(120)
+
+sam.left(85)
+sam.forward(110)
+
+sam.left(72)
+sam.forward(180)
+screen.exitonclick()
